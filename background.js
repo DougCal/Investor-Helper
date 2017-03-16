223868256753fbeafec94406303072c7df73849e
Ryan Fowlkes Smith <ryan.fowlkes@gmail.com>
2017-03-15 21:43:51 -0700

    woot woot woot woot best chrome extension OF ALL TIME WE ARE GOAT

--- background.js
+++ background.js
@@ -1,11 +1,44 @@
 // A function to use as callback
-function searchDom(domContent) {
-    console.log(domContent);
+function clickAction(domContent) {
+
+    let newDom = domContent.replace(/<|>|"/, '\\$&');
+    console.log(JSON.parse(newDom));
+
+
+
+
+
+
+
+
+    domContent.forEach(node => {
+      if (node.style.fontFamily) node.style.fontFamily = "Arial";
+    })
+    // domContent.onClick = function(){
+    //   console.log('You Clicked Me!');
+    // };
+    // console.log('domContent.onClick:', domContent.onClick);
 }
 
-// When the browser-action button is clicked...
-chrome.browserAction.onClicked.addListener(function (tab) {
-  console.log('registered click on background.js');
 
-  chrome.tabs.sendMessage(tab.id, {text: 'woohoo'}, searchDom);
+chrome.browserAction.onClicked.addListener(function (tab) {
+  chrome.tabs.sendMessage(tab.id, {message: 'blar'}, clickAction);
 });
+
+// // Regex-pattern to check URLs against.
+// // It matches URLs like: http[s]://[...]stackoverflow.com[...]
+// var urlRegex = /^https?:\/\/(?:[^./?#]+\.)?stackoverflow\.com/;
+//
+// // A function to use as callback
+// function doStuffWithDom(domContent) {
+//     console.log('I received the following DOM content:\n' + domContent);
+// }
+//
+// // When the browser-action button is clicked...
+// chrome.browserAction.onClicked.addListener(function (tab) {
+//     // ...check the URL of the active tab against our pattern and...
+//     if (urlRegex.test(tab.url)) {
+//         // ...if it matches, send a message specifying a callback too
+//         chrome.tabs.sendMessage(tab.id, {text: 'report_back'}, doStuffWithDom);
+//     }
+// });
--- content.js
+++ content.js
@@ -1,18 +1,63 @@
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     // If the received message has the expected format...
-    if (msg.text === 'woohoo') {
+      const domArr = $(document).find('*');
 
-        // Call the specified callback, passing
-        // the web-page's DOM content as argument
-        //sendResponse(document.all);
-    }
+
+      sendResponse(document.all[0].outerHTML);
+
 });
 
-document.body.style.fontFamily = 'Sans-Serif';
 
-console.log(document.body.style);
+Array.from($(document).find('*')).forEach(node => {
+  //console.log(node.style.fontFamily === "");
+  node.style.fontFamily = "Arial";
+});
 
 
+
+
+
+
+
+// // Listen for messages
+// chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+//     // If the received message has the expected format...
+//     if (msg.text === 'report_back') {
+//         // Call the specified callback, passing
+//         // the web-page's DOM content as argument
+//         sendResponse(document.all[0].outerHTML);
+//     }
+// });
+
+
+
+
+
+
+
+
+
+// // $( document ).mousemove((e) => {
+// //   console.log('x-coord:', e.pageX, 'y-coord:', e.pageY);
+// // })
+//
+// const doc = Array.from($( document ).find('*'));
+//
+// const divs = Array.from($(document).find('div'));
+//
+// divs.forEach(div => {
+//   div.onClick = function(){
+//     // console.log('click!!!');
+//   }
+//   // console.log(div.onClick);
+// })
+//
+// // doc.forEach(node => {
+// //   // if (node.tagName === "P") node.style.backgroundColor = "yellow";
+// //   node.style.fontFamily = "Sans-Serif";
+// //   node.style.fontStyle = "normal";
+// //   console.log(node.tagName);
+// // });
 
 
 
